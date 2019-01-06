5de25bc1b4ab4c9991cf76d858c9c56324644290
Even <even.199010@gmail.com>
2019-01-06 17:12:20 +0800

    Initial Commit

---
+++ CustomLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLibrary
+//
+//  Created by Even on 2019/1/6.
+//  Copyright © 2019年 EvenZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomLibrary
+//
+//  Created by Even on 2019/1/6.
+//  Copyright © 2019年 EvenZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
